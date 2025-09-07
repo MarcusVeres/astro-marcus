e886ed3cf37da9a9f343e6182b210528976674f1
Marcus Veres <marcus.veres@gmail.com>
2025-09-07 17:19:45 -0400

    imports tailwind CSS into the project! - use tailwind locally instead of CDN version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://astro-tutorial-1-good-boy-america.netlify.app",
-  integrations: [preact()]
 });
